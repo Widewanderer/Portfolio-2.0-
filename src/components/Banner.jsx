c0ee79005b3c8ff224ff1a60e3a0a2a87f6ce294
Widewanderer <slavatrotz@gmail.com>
2023-12-08 14:47:40 -0600

    added blurred background to banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,12 @@
 export default function Banner() {
+  const bannerStyle = {
+    boxShadow: "0 0 10px 10px rgba(0, 0, 0, 0.4)",
+    padding: "10px",
+  };
   return (
-    // create border and blur border  
-    <div className="banner">
-        {/* could conditionally render information here (if esle statement)
+    // create border and blur border
+    <div className="banner" style={bannerStyle}>
+      {/* could conditionally render information here (if esle statement)
         Alternatively, could just use a h2, h3, p tag, etc  */}
     </div>
   );
