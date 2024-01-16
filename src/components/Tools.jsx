6c9b6f9c1dad9af4e6451158c02ee0d03d9332b2
Widewanderer <slavatrotz@gmail.com>
2024-01-15 22:14:55 -0600

    modified felx boxes and (skill presentation grid) asthetics improved. However refactoring to use CSS grid format will be needed.

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -24,15 +24,72 @@ export default function DisplayTools() {
       name: "MongoDB",
       src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg",
     },
+    {
+      name: "MongoDB",
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg",
+    },
+    {
+      name: "MongoDB",
+      src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg",
+    },
   ];
-  return(
-    <>
-    {skills.map(skill => (
-        <>
-        <img className="toolImage" src={skill.src} /> 
-        <p>{skill.name}</p>
-        </>
-    ))}
-    </>
-  )
+
+  const outerContainerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    // height: "140px", // Adjust as needed
+    width: "100%",
+  };
+
+  const innerContainerStyle = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "5px",
+    width: "50%",
+    padding: "5px",
+    borderRadius: "10px",
+    backgroundColor: "#0E3608",
+    // backgroundColor: "#1C4E15",
+  };
+
+  const skillStyle = {
+    marginBottom: "8px", // Adjust as needed
+    textAlign: "center",
+    margin: "0 15px",
+    padding: "15px",
+    margin: "5px 5px",
+    width: "23%",
+    borderRadius: "10px",
+    backgroundColor: "#051902",
+  };
+
+  // could modify "toolImage" to directly change size of SRCs 
+  
+// 
+
+  return (
+    <div style={outerContainerStyle}>
+      <div style={innerContainerStyle}>
+        {skills.map((skill, index) => (
+          <div key={index} style={skillStyle}>
+            <img className="toolImage" src={skill.src} alt={skill.name} />
+            {/* <p>{skill.name}</p> */}
+          </div>
+        ))}
+      </div>
+    </div>
+
+    //   return(
+    //     <>
+    //     {skills.map(skill => (
+    //         <>
+    //         <img className="toolImage" src={skill.src} />
+    //         <p>{skill.name}</p>
+    //         </>
+    //     ))}
+    //     </>
+  );
 }
