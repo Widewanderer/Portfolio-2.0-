fb50ae72768612ce13c85572715b154db91e3e74
Widewanderer <slavatrotz@gmail.com>
2024-01-21 21:51:38 -0600

    further styling adjustments to footer, margins, etc

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,42 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const footerStyle = {
-  height: "50px",
+  height: "75px",
   padding: "15px",
-  marginTop: "10px",
+  marginTop: "40px",
   backgroundColor: "#232323", // Add a background color for visibility
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
-//   position: "fixed",
-//   bottom: 0, // Fix position to bottom of viewport
   width: "100%",
+  flexDirection: "column",
+  textAlign: "center",
 };
+
+const textFooterStyle = {
+  width: "60%",
+  margin: "10px auto",
+};
+
+const footerText = {
+  color: "#E2E1D6",
+};
+
 
 const Footer = () => {
     const location = useLocation();
     const navigate = useNavigate();
     return (
-        <footer class= "footer" style={footerStyle}>
-            <h6 style={{color:"#FFF"}}>
-                The WideWanderer
-
-            </h6>
-
-        </footer>
-    )
+      <footer class="footer" style={footerStyle}>
+        <div style={textFooterStyle}>
+          <h6 style={footerText}>
+            “Never let the future disturb you. You will meet it, if you have to,
+            with the same weapons of reason which today arm you against the
+            present.”
+          </h6>
+        </div>
+      </footer>
+    );
 }
 
 export default Footer; 
