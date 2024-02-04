86d3ca4dc797daf6bfdf3142e2c85561cf7481cc
Widewanderer <slavatrotz@gmail.com>
2024-02-03 23:09:41 -0600

    began componet to display profile picture

---
+++ src/components/Picture.jsx
@@ -0,0 +1,7 @@
+import { useLocation, useNavigate } from "react-router-dom";
+
+export default function ProfilePic() {
+    <div> 
+        <img src = {require("../assets/HikingPic.jpg").default} alt = "Profile Picture" />
+    </div>
+}
