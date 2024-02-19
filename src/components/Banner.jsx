2a4261b3869a7f954ca57b942a0ee900ef33947f
Widewanderer <slavatrotz@gmail.com>
2024-02-18 22:29:57 -0600

    modify color of JavaScript SVG repea & test for others. decreased banner size, add minHeight so profile picture can not be larger than banner.

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import HikingPic from "../assets/profile/HikingPic.png";
 import MidJourney from "../assets/profile/MidJourney.png";
 
 export default function Banner() {
-  const bannerStyle = {
+  const bannerStyle = { // image & additional styling for banner in App.css 
     position: "relative", 
     boxShadow: "0 0 10px 10px rgba(0, 0, 0, 0.4)",
     padding: "10px",
@@ -12,29 +12,17 @@ export default function Banner() {
     position: "absolute", // Position the photo absolutely within the banner
     top: "5vh", 
     left: "5vw", 
-    width: "120px", 
+    width: "100px", 
     height: "auto", // Maintain aspect ratio
     borderRadius: "50%",
   };
 
   return (
-    <div className="banner" style={bannerStyle}>
-      {/* Display the photo */}
-      <img src={MidJourney} alt="Profile Picture" style={photoStyle} />
+    <div className="banner" style={bannerStyle}> 
+      <img src={MidJourney} alt="Profile Picture" style={photoStyle} /> 
+      {/* Profile Picture within the banner */}
     </div>
   );
 }
 
-// export default function Banner() {
-//   const bannerStyle = {
-//     boxShadow: "0 0 10px 10px rgba(0, 0, 0, 0.4)",
-//     padding: "10px",
-//   };
-//   return (
-//     // create border and blur border
-//     <div className="banner" style={bannerStyle}>
-//       {/* could conditionally render information here (if esle statement)
-//         Alternatively, could just use a h2, h3, p tag, etc  */}
-//     </div>
-//   );
-// }
+
