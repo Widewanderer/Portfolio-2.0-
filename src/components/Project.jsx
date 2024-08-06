a01c9f13aa6f9e4e3898daf87aa72128661eb186
Widewanderer <slavatrotz@gmail.com>
2024-08-06 18:03:28 -0500

    update culturedCuisine and WUR photos for be uniform in size

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,11 +1,20 @@
 import * as projects from "../assets";
+import '../App.css';
+
+
+const pImageStyle = {
+  width: "300px",
+  height: "200px",
+  objectFit: "cover",
+  display: "block"
+} 
 
 function Project({ project }) {
   const { name, repo, link, description, image } = project;
 
   return (
     <div className="p-3">
-      <img src={projects[image]} alt={name} className="p-img" />
+      <img src={projects[image]} alt={name} style={pImageStyle}/>
       <div className="">
         <h1>
           <a href={link}>{name}</a>{" "}
